62062d9c3ab6ec33db61307a597837f4e254a10e
bruh.ngl0 <adityashrm500@gmail.com>
2024-05-30 20:45:01 +0530

    life is good anime

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,14 +4,14 @@ import '../styles/project.scss';
 import '../../public/cover.png'
 
 const projects = [
-  { id: 1, title: 'VIRTUAL CARDS', description: 'SEE MORE.', image: 'cover.png'},
-  { id: 2, title: 'BLOG PLATFORM', description: 'SEE MORE' , image: 'cover.png'},
-  { id: 3, title: 'SAAS SERVICE', description: 'SEE MORE' , image: 'cover.png'},
-  { id: 4, title: 'INVEST PLATFORM', description: 'SEE MORE' , image: 'cover.png'},
-  { id: 5, title: 'VIRTUAL CARDS', description: 'SEE MORE.', image: 'cover.png'},
-  { id: 6, title: 'BLOG PLATFORM', description: 'SEE MORE' , image: 'cover.png'},
-  { id: 7, title: 'SAAS SERVICE', description: 'SEE MORE' , image: 'cover.png'},
-  { id: 8, title: 'INVEST PLATFORM', description: 'SEE MORE' , image: 'cover.png'},
+  { id: 1, title: 'LUFFY MONKE', description: 'SEE MORE.', image: 'luffy.png'},
+  { id: 2, title: 'NAMI MAMA', description: 'SEE MORE' , image: 'nami.png'},
+  { id: 3, title: 'RONOROA ZORO', description: 'SEE MORE' , image: 'zoro.png'},
+  { id: 4, title: 'CHEF SANJI', description: 'SEE MORE' , image: 'sanji.png'},
+  { id: 5, title: 'DOC CHOP', description: 'SEE MORE.', image: 'chopper.png'},
+  { id: 6, title: 'BUGGI PIRATE', description: 'SEE MORE' , image: 'buggi.png'},
+  { id: 7, title: 'WARRIOR SNAKE', description: 'SEE MORE' , image: 'snake.png'},
+  { id: 8, title: 'MUSHAI', description: 'SEE MORE' , image: 'cover.png'},
   
   
 ];
