9a8354ba5559c4185671e6991dcfcce6c3426c45
bruh.ngl0 <adityashrm500@gmail.com>
2024-06-07 12:26:34 +0530

    scroll

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,32 +1,30 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import '../styles/project.scss';
 import '../../public/cover.png';
 
 const projects = [
-  { id: 1, title: 'LUFFY MONKE', description: 'SEE MORE.', image: 'luffy.png' },
-  { id: 2, title: 'NAMI MAMA', description: 'SEE MORE', image: 'nami.png' },
-  { id: 3, title: 'RONOROA ZORO', description: 'SEE MORE', image: 'zoro.png' },
-  { id: 4, title: 'CHEF SANJI', description: 'SEE MORE', image: 'sanji.png' },
-  { id: 5, title: 'DOC CHOP', description: 'SEE MORE.', image: 'chopper.png' },
-  { id: 6, title: 'BUGGI PIRATE', description: 'SEE MORE', image: 'buggi.png' },
-  { id: 7, title: 'WARRIOR SNAKE', description: 'SEE MORE', image: 'snake.png' },
-  { id: 8, title: 'MUSHAI', description: 'SEE MORE', image: 'cover.png' },
+  { id: 1, title: 'LUFFY MONKE', description: 'SEE MORE.', image: 'luffy.png'},
+  { id: 2, title: 'NAMI MAMA', description: 'SEE MORE' , image: 'nami.png'},
+  { id: 3, title: 'RONOROA ZORO', description: 'SEE MORE' , image: 'zoro.png'},
+  { id: 4, title: 'CHEF SANJI', description: 'SEE MORE' , image: 'sanji.png'},
+  { id: 5, title: 'DOC CHOP', description: 'SEE MORE.', image: 'chopper.png'},
+  { id: 6, title: 'BUGGI PIRATE', description: 'SEE MORE' , image: 'buggi.png'},
+  { id: 7, title: 'WARRIOR SNAKE', description: 'SEE MORE' , image: 'snake.png'},
+  { id: 8, title: 'MUSHAI', description: 'SEE MORE' , image: 'cover.png'},
 ];
 
 const Project = () => {
-  const [visibleProjects, setVisibleProjects] = useState([]);
-
   useEffect(() => {
     const handleScroll = () => {
       const projectCards = document.querySelectorAll('.project-card');
-      const visible = [];
       projectCards.forEach((card, index) => {
         const rect = card.getBoundingClientRect();
         if (rect.top < window.innerHeight && rect.bottom >= 0) {
-          visible.push(index);
+          card.classList.add('show');
+        } else {
+          card.classList.remove('show');
         }
       });
-      setVisibleProjects(visible);
     };
 
     const debouncedHandleScroll = debounce(handleScroll, 100);
@@ -39,12 +37,9 @@ const Project = () => {
   return (
     <div className="projects-container">
       {projects.map((project, index) => (
-        <div
-          key={project.id}
-          className={`project-card project-${index + 1} ${visibleProjects.includes(index) ? 'show' : ''}`}
-        >
+        <div key={project.id} className={`project-card project-${index + 1}`}>
           <h2>{project.title}</h2>
-          <img src={project.image} alt={project.title} />
+          <img src={project.image} alt={project.title}/>
           <p>{project.description}</p>
         </div>
       ))}
@@ -55,7 +50,7 @@ const Project = () => {
 // Debounce function to limit the rate of function calls
 function debounce(func, wait) {
   let timeout;
-  return function (...args) {
+  return function(...args) {
     const later = () => {
       clearTimeout(timeout);
       func(...args);
