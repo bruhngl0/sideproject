13e31307a2f3f6daf15a78b116af9bc55f519dd5
bruh.ngl0 <adityashrm500@gmail.com>
2024-06-22 20:06:12 +0530

    photo

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -3,15 +3,18 @@ import Header from './Header'
 import Project from './Project'
 import Footer from './Footer'
 import TextReveal from './TextReveal'
-
+import PhotographyPage from './PhotographyPage'
 import '../../src/styles/header.scss'
 import '../../src/styles/footer.scss'
+import '../../src/styles/photography.scss'
+
 const Homepage = () => {
   return (
     <div>
       <Header />
-     
+       <PhotographyPage />
       <Project />
+      <TextReveal />
     
       
      
