3bf3e82d492bd608747a60079065b21ebef9510c
bruh.ngl0 <adityashrm500@gmail.com>
2024-07-17 18:08:56 +0530

    des

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './Header'
 import Project from './Project'
+
 import Footer from './Footer'
 import TextReveal from './TextReveal'
 import PhotographyPage from './PhotographyPage'
 import '../../src/styles/header.scss'
 import '../../src/styles/footer.scss'
 import '../../src/styles/photography.scss'
+
 
 const Homepage = () => {
   return (
--- src/components/TextReveal.jsx
+++ src/components/TextReveal.jsx
@@ -8,6 +8,12 @@ const texts = [
   'Nisi ut aliquip ex ea commodo consequat.',
   'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore.',
   'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt.',
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.',
+  'Nisi ut aliquip ex ea commodo consequat.',
+  'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore.',
+  'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt.',
 ];
 
 const TextReveal = () => {
