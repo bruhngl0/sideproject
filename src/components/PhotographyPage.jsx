ac92b7532606fa5ba0d545c61e7f7e136e84a871
bruh.ngl0 <adityashrm500@gmail.com>
2024-08-10 17:43:31 +0530

    tap

--- src/components/PhotographyPage.jsx
+++ src/components/PhotographyPage.jsx
@@ -12,10 +12,12 @@ const PhotographyPage = () => {
         <p className='para-po'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget arcu et enim mattis tristique. Nullam a leo aliquet, tincidunt nibh sit amet, pellentesque mauris. Nunc quis tellus posuere, fermentum felis at, tristique leo. Praesent nec facilisis turpis. Praesent sit amet velit vitae libero mollis efficitur. Praesent rutrum erat quis facilisis dapibus. Curabitur nec tempus nibh, ac eleifend turpis.</p>
         <p className='para-po'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget arcu et enim mattis tristique. Nullam a leo aliquet, tincidunt nibh sit amet, pellentesque mauris. Nunc quis tellus posuere, fermentum felis at, tristique leo. Praesent nec facilisis turpis. Praesent sit amet velit vitae libero mollis efficitur. Praesent rutrum erat quis facilisis dapibus. Curabitur nec tempus nibh, ac eleifend turpis.</p>
         <p className='para-po'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget arcu et enim mattis tristique. Nullam a leo aliquet, tincidunt nibh sit amet, pellentesque mauris. Nunc quis tellus posuere, fermentum felis at, tristique leo. Praesent nec facilisis turpis. Praesent sit amet velit vitae libero mollis efficitur. Praesent rutrum erat quis facilisis dapibus. Curabitur nec tempus nibh, ac eleifend turpis.</p>
-       
-        <a href="#text-reveal">
-        <img src="arrowdown.png" />
-        </a>     
+         
+
+         <p>tap and scroll</p>
+        {/*<a href="#text-reveal">
+        //<img src="arrowdown.png" />
+        </a> */}     
       </section>
 
     </main>
