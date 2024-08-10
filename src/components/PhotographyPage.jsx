fba6f58381c6469b914c78e373b2ab31a388e30b
bruh.ngl0 <adityashrm500@gmail.com>
2024-08-10 23:26:18 +0530

    tex

--- src/components/PhotographyPage.jsx
+++ src/components/PhotographyPage.jsx
@@ -7,18 +7,29 @@ const PhotographyPage = () => {
       <header className='joker-one'></header>
       <section className='joker-three'>
        
+        <div className='utils'>
+          <div className='utils-one'>
+            <span>UI/UX Design</span>
+            <span>Database Management</span>
+            <span>Debugging</span>
+          </div>
 
-        <img className='brain-img'  src="lo.jpg"/>
+          <div className='utils-two'>
+             <span>Resume</span>
+          </div>
+        </div>
+        <img className='brain-img'  src="bts.jpg"/>
 
-      
+        <div className='para-po-main'>      
         <p className='para-po'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget arcu et enim mattis tristique. Nullam a leo aliquet, tincidunt nibh sit amet, pellentesque mauris. Nunc quis tellus posuere, fermentum felis at, tristique leo. Praesent nec facilisis turpis. Praesent sit amet velit vitae libero mollis efficitur. Praesent rutrum erat quis facilisis dapibus. Curabitur nec tempus nibh, ac eleifend turpis.</p>
         <p className='para-po'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget arcu et enim mattis tristique. Nullam a leo aliquet, tincidunt nibh sit amet, pellentesque mauris. Nunc quis tellus posuere, fermentum felis at, tristique leo. Praesent nec facilisis turpis. Praesent sit amet velit vitae libero mollis efficitur. Praesent rutrum erat quis facilisis dapibus. Curabitur nec tempus nibh, ac eleifend turpis.</p>
         <p className='para-po'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. FuscePraesent rutrum erat quis facilisis dapibus. Curabitur nec tempus nibh, ac eleifend turpis.</p>
         <p className='para-po'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget arcu et enim mattis tristique. Nullam a leo aliquet, tincidunt nibh sit amet, pellentesque mauris. Nunc quis tellus posuere, fermentum felis at, tristique leo. Praesent nec facilisis turpis. Praesent sit amet velit vitae libero mollis efficitur. Praesent rutrum erat quis facilisis dapibus. Curabitur nec tempus nibh, ac eleifend turpis.</p>
+        </div>
         <a href="#text-reveal">
         <img src="arrowdown.png" />
         </a> 
-        
+       
            
       </section>
 
