8b88e0ddf67b502ba128f7a48c69db1279529342
bruh.ngl0 <adityashrm500@gmail.com>
2024-08-10 17:31:10 +0530

    load

--- src/components/PhotographyPage.jsx
+++ src/components/PhotographyPage.jsx
@@ -11,7 +11,8 @@ const PhotographyPage = () => {
         <img className='brain-img'  src="sam.jpg"/>
         <p className='para-po'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget arcu et enim mattis tristique. Nullam a leo aliquet, tincidunt nibh sit amet, pellentesque mauris. Nunc quis tellus posuere, fermentum felis at, tristique leo. Praesent nec facilisis turpis. Praesent sit amet velit vitae libero mollis efficitur. Praesent rutrum erat quis facilisis dapibus. Curabitur nec tempus nibh, ac eleifend turpis.</p>
         <p className='para-po'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget arcu et enim mattis tristique. Nullam a leo aliquet, tincidunt nibh sit amet, pellentesque mauris. Nunc quis tellus posuere, fermentum felis at, tristique leo. Praesent nec facilisis turpis. Praesent sit amet velit vitae libero mollis efficitur. Praesent rutrum erat quis facilisis dapibus. Curabitur nec tempus nibh, ac eleifend turpis.</p>
-        
+        <p className='para-po'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget arcu et enim mattis tristique. Nullam a leo aliquet, tincidunt nibh sit amet, pellentesque mauris. Nunc quis tellus posuere, fermentum felis at, tristique leo. Praesent nec facilisis turpis. Praesent sit amet velit vitae libero mollis efficitur. Praesent rutrum erat quis facilisis dapibus. Curabitur nec tempus nibh, ac eleifend turpis.</p>
+       
         <a href="#text-reveal">
         <img src="arrowdown.png" />
         </a>     
