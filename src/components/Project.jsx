d6be9ba6ed8e9c3dad9a786833fa78b5860aabfa
bruh.ngl0 <adityashrm500@gmail.com>
2024-08-11 16:51:55 +0530

    plain

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,15 +3,15 @@ import '../styles/project.scss';
 import coverImage from '../../public/cover.png'; // Adjust the path based on your setup
 
 const projects = [
-  { id: 1, title: 'LUFFY MONKE', description: 'SEE MORE.', image: 'one.png' },
-  { id: 2, title: 'NAMI MAMA', description: 'SEE MORE', image: 'two.png' },
-  { id: 3, title: 'RONOROA ZORO', description: 'SEE MORE', image: 'three.png' },
-  { id: 4, title: 'CHEF SANJI', description: 'SEE MORE', image: 'four.png' },
-  { id: 5, title: 'DOC CHOP', description: 'SEE MORE.', image: 'five.png' },
-  { id: 6, title: 'BUGGI PIRATE', description: 'SEE MORE', image: 'six.png' },
-  { id: 7, title: 'WARRIOR SNAKE', description: 'SEE MORE', image: 'one.png' },
-  { id: 8, title: 'MUSHAI', description: 'SEE MORE', image: 'two.png' },
-];
+  { id: 1, title: '', description: '', image: '' },
+  { id: 2, title: '', description: '', image: '' },
+  { id: 3, title: '', description: '', image: '' },
+  { id: 4, title: '', description: '', image: '' },
+  { id: 5, title: '', description: '', image: '' },
+  { id: 6, title: '', description: '', image: '' },
+  { id: 7, title: '', description: '', image: '' },
+  { id: 8, title: '', description: '', image: '' },
+]; 
 
 const Project = () => {
   const projectRefs = useRef([]);
