ee214fef2aeed859beb78b3f3f3b9879f4e4d151
bruh.ngl0 <adityashrm500@gmail.com>
2024-08-11 18:48:59 +0530

    plain

--- src/components/PhotographyPage.jsx
+++ src/components/PhotographyPage.jsx
@@ -18,7 +18,7 @@ const PhotographyPage = () => {
              <span>Resume</span>
           </div>
         </div>
-        <img className='brain-img'  src="roses1.jpg"/>
+        <img className='brain-img'  src="bts.jpg"/>
 
         <div className='para-po-main'>      
         <p className='para-po'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget arcu et enim mattis tristique. Nullam a leo aliquet, tincidunt nibh sit amet, pellentesque mauris. Nunc quis tellus posuere, fermentum felis at, tristique leo. Praesent nec facilisis turpis. Praesent sit amet velit vitae libero mollis efficitur. Praesent rutrum erat quis facilisis dapibus. Curabitur nec tempus nibh, ac eleifend turpis.</p>
