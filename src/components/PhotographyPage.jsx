4b6739032d53ddadf9d9fe223639d27d86481699
bruh.ngl0 <adityashrm500@gmail.com>
2024-08-13 11:36:16 +0530

    alaala

--- src/components/PhotographyPage.jsx
+++ src/components/PhotographyPage.jsx
@@ -18,24 +18,32 @@ const PhotographyPage = () => {
              <span>Resume</span>
           </div>
         </div>
-        <img className='brain-img'  src="watch.jpg"/>
+        <img className='brain-img'  src="arr.jpg"/>
 
         <div className='para-po-main'>      
-        <p className='para-po'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget arcu et enim mattis tristique. Nullam a leo aliquet, tincidunt nibh sit amet, pellentesque mauris. Nunc quis tellus posuere, fermentum felis at, tristique leo. Praesent nec facilisis turpis. Praesent sit amet velit vitae libero mollis efficitur. Praesent rutrum erat quis facilisis dapibus. Curabitur nec tempus nibh, ac eleifend turpis.</p>
+        <p className='para-po'>(About Series)</p>
         
         
          
         
-       <p className='para-po'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget arcu et enim mattis tristique. Nullam a leo aliquet, tincidunt nibh sit amet, pellentesque mauris. Nunc quis tellus posuere, fermentum felis at, tristique leo. Praesent nec facilisis turpis. Praesent sit amet velit vitae libero mollis efficitur. Praesent rutrum erat quis facilisis dapibus. Curabitur nec tempus nibh, ac eleifend turpis.</p>
+       
+       <p className='para-po-big'> Design Education Series is a new format of an original mini-series on the main principles of design, where we share all insights gained during our experience at Obys Agency. Typography Principles, Colors Combinations, Grids are the titles of the first three seasons of the series.</p>
+       <p className='para-po-small' id="para-po"> Over the past 5 years, Obys has released 3 educational projects (websites) that have become quite popular within the design community. These websites have been visited over 1 million times.</p>
+       <p className='para-po-small'> Here is the new, light, and entertaining format of the series that we are excited to present to you. With short episodes full of valuable information without fluff, these will teach you the main principles of design and enhance your skills in working with typography, grids, colors, and composition.</p>
        <a href= "#para-po" >
        <img src="arrowdown.png"  className='king'/>
        </a>
-        <p className='para-po' id="para-po">Lorem ipsum dolor sit amet, consectetur adipiscing elit. FuscePraesent rutrum erat quis facilisis dapibus. Curabitur nec tempus nibh, ac eleifend turpis.</p>
-        
-        <p className='para-po'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget arcu et enim mattis tristique. Nullam a leo aliquet, tincidunt nibh sit amet, pellentesque mauris. Nunc quis tellus posuere, fermentum felis at, tristique leo. Praesent nec facilisis turpis. Praesent sit amet velit vitae libero mollis efficitur. Praesent rutrum erat quis facilisis dapibus. Curabitur nec tempus nibh, ac eleifend turpis.</p>
-        </div>
+       
         
        
+        </div>
+        
+       <div className='list'>
+          <span>Projects</span>
+          <span>Typtgraphy Principles(2019)</span>
+          <span>Color Combinations(2021)</span>
+          <span>Grids(coming soon)</span>
+       </div>
            
       </section>
 
