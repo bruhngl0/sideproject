e28a9df39ba50e8412ac10014c3e3fa0c7f8d19c
bruh.ngl0 <adityashrm500@gmail.com>
2024-08-17 18:50:05 +0530

    cards

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,11 +3,11 @@ import '../styles/project.scss';
 import coverImage from '../../public/cover.png'; // Adjust the path based on your setup
 
 const projects = [
-  { id: 1, title: '', description: '', image: '' },
-  { id: 2, title: '', description: '', image: '' },,
-  { id: 3, title: '', description: '', image: '' },
-  { id: 4, title: '', description: '', image: '' },
-  { id: 5, title: '', description: '', image: '' },
+  { id: 1, title: 'UNNECESSARY', description: 'Here is the new, light, and entertaining format of the series that we are excited to present to you format of the series that we are excited to', name: 'e-comm', image: 'ripley.jpg', description1:'we live lit', description2: 'test description two', util1: '', util2: ''},
+  { id: 2, title: 'UNNECESSARY', description: 'Here is the new, light, and entertaining format of the series that we are excited to present to you format of the series that we are excited to', name: 'e-comm', image: 'ripley2.jpg', description1:'we live lit', description2: 'test description two', util1: '', util2: ''},
+  { id: 3, title: 'UNNECESSARY', description: 'Here is the new, light, and entertaining format of the series that we are excited to present to you format of the series that we are excited to', name: 'e-comm', image: 'ripley3.jpg', description1:'we live lit', description2: 'test description two', util1: '', util2: ''},
+  { id: 4, title: 'UNNECESSARY', description: 'Here is the new, light, and entertaining format of the series that we are excited to present to you format of the series that we are excited to', name: 'e-comm', image: 'ripley4.jpg', description1:'we live lit', description2: 'test description two', util1: '', util2: ''},
+  { id: 5, title: 'UNNECESSARY', description: 'Here is the new, light, and entertaining format of the series that we are excited to present to you format of the series that we are excited to', name: 'e-comm', image: 'ripley5.jpg', description1:'we live lit', description2: 'test description two', util1: '', util2: ''},
  ,
 ]; 
 
@@ -53,9 +53,12 @@ const Project = () => {
           ref={(el) => (projectRefs.current[index] = el)}
           className="project-card"
         >
-          <h2>{project.title}</h2>
+          <h2 className='title'>{project.title}</h2>
+          <p className='des'>{project.description}</p>
+          <span className='name'>{project.name}</span>
           <img src={project.image} alt={project.title} className='proj-img'/>
-          <p>{project.description}</p>
+          <p className='desc-1'>{project.description1}</p>
+          <p className='desc-2'>{project.description2}</p>
         </div>
       ))}
     </div>
