f526b39f8557c730b74dbf50bd2a335aabfd66d4
bruhngl0 <adityashrm500@gmail.com>
2025-02-25 22:35:59 +0530

    port

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import React from "react"
 import Homepage from "./components/Homepage"
+import { BrowserRouter as Router, Routes , Route  } from "react-router-dom"
+import Test from "./components/Test"
 function App() {
   
 
   return (
     <>
-     <Homepage />
+    <Router>
+      <Routes>
+        <route to= "/" element = {<Test/>} />
+      </Routes>
+    </Router>
+
     </>
   )
 }
